04146de361678e315c88a10f8a4fd120dbd0a51b
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-06-03 13:36:57 -0400

    streaming and seo bases

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,18 @@
+import { Comments } from "@/ui/Comments";
+import { Todos } from "@/ui/Todos";
+import { Suspense } from "react";
 import styles from "./dashboard.module.css";
 
 export default function Dashboard() {
   return (
     <div className={styles.dashboard}>
-      Hello am Dashboard!
+      <h1>Dashboard</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Todos />
+      </Suspense>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Comments />
+      </Suspense>
     </div>
   );
 }
